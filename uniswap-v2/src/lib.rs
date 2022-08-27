43919d93287f8e1fa501d32a211ba38f3fd090e5
Vincent Wen <wensi.isnew@gmail.com>
2022-08-27 09:59:50 -0700

    Refactored schema types

--- uniswap-v2/src/lib.rs
+++ uniswap-v2/src/lib.rs
@@ -1,15 +1,20 @@
-use hex;
+use hex_literal::hex;
 use substreams::{log, proto, store};
 use substreams_ethereum::{Event as EventTrait, pb::eth::v2 as eth};
+use substreams_helper::{erc20, utils};
 
 use abi::factory;
+
+use pb::common;
+use pb::dex_amm;
 use pb::uniswap_v2;
-use substreams_helper::{erc20, utils};
 
 mod abi;
 mod pb;
 
-pub const UNISWAP_V2_FACTORY: &str = "5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
+type Address = [u8; 20];
+
+pub const UNISWAP_V2_FACTORY: Address = hex!("5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f");
 pub const UNISWAP_V2_FACTORY_START_BLOCK: u64 = 10_000_835;
 
 trait BlockExt {
@@ -34,7 +39,7 @@ fn map_pair_created_event(block: eth::Block) -> Result<uniswap_v2::PairCreatedEv
 
     for log in block.logs() {
         if let Some(event) = factory::events::PairCreated::match_and_decode(log) {
-            if hex::encode(&log.log.address) != UNISWAP_V2_FACTORY {
+            if log.log.address != UNISWAP_V2_FACTORY {
                 continue;
             }
 
@@ -64,8 +69,8 @@ fn store_pair_created_event(pair_created_events: uniswap_v2::PairCreatedEvents, 
 }
 
 #[substreams::handlers::map]
-fn map_pair(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<uniswap_v2::Pairs, substreams::errors::Error> {
-    let mut pairs = uniswap_v2::Pairs { items: vec![] };
+fn map_pool(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<dex_amm::Pools, substreams::errors::Error> {
+    let mut pools = dex_amm::Pools { items: vec![] };
 
     for event in pair_created_events.items {
         match erc20::get_erc20_token(&event.token0) {
@@ -77,7 +82,7 @@ fn map_pair(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<uniswa
             }
         }
 
-        pairs.items.push(uniswap_v2::Pair {
+        pools.items.push(dex_amm::Pool {
             name: event.pair.clone(),
             token0: event.token0,
             token1: event.token1,
@@ -85,5 +90,5 @@ fn map_pair(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<uniswa
         })
     }
 
-    Ok(pairs)
+    Ok(pools)
 }
--- uniswap-v2/src/pb/mod.rs
+++ uniswap-v2/src/pb/mod.rs
@@ -1,2 +1,6 @@
+#[path = "messari.common.rs"]
+pub mod common;
+#[path = "messari.dex_amm.rs"]
+pub mod dex_amm;
 #[path = "messari.uniswap_v2.rs"]
 pub mod uniswap_v2;
