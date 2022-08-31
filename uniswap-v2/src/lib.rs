021cfc05d1aa974dd29c3eb20d37396fe2f0d54d
Vincent Wen <wensi.isnew@gmail.com>
2022-08-30 22:35:48 -0700

    feat(uniswap-v2): refactored protobuf definitions; added more events (#12)

--- uniswap-v2/src/lib.rs
+++ uniswap-v2/src/lib.rs
@@ -1,15 +1,21 @@
-use hex;
+use hex_literal::hex;
 use substreams::{log, proto, store};
 use substreams_ethereum::{Event as EventTrait, pb::eth::v2 as eth};
+use substreams_helper::{erc20, utils};
 
 use abi::factory;
+
+use pb::common;
+use pb::dex_amm;
 use pb::uniswap_v2;
-use substreams_helper::{erc20, utils};
 
 mod abi;
 mod pb;
 
-pub const UNISWAP_V2_FACTORY: &str = "5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
+type Address = [u8; 20];
+
+pub const UNISWAP_V2_FACTORY: Address = hex!("5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f");
+pub const UNISWAP_V2_FACTORY_START_BLOCK: u64 = 10_000_835;
 
 trait BlockExt {
     fn timestamp(&self) -> u64;
@@ -33,7 +39,7 @@ fn map_pair_created_event(block: eth::Block) -> Result<uniswap_v2::PairCreatedEv
 
     for log in block.logs() {
         if let Some(event) = factory::events::PairCreated::match_and_decode(log) {
-            if hex::encode(&log.log.address) != UNISWAP_V2_FACTORY {
+            if log.log.address != UNISWAP_V2_FACTORY {
                 continue;
             }
 
@@ -41,8 +47,8 @@ fn map_pair_created_event(block: eth::Block) -> Result<uniswap_v2::PairCreatedEv
                 token0: hex::encode(event.token0),
                 token1: hex::encode(event.token1),
                 pair: hex::encode(event.pair),
-                block_number: block.number,
-                block_timestamp: block.timestamp(),
+                tx_hash: hex::encode(log.receipt.transaction.hash.clone()),
+                log_index: log.index(),
             })
         }
     }
@@ -52,7 +58,7 @@ fn map_pair_created_event(block: eth::Block) -> Result<uniswap_v2::PairCreatedEv
 
 #[substreams::handlers::store]
 fn store_pair_created_event(pair_created_events: uniswap_v2::PairCreatedEvents, output: store::StoreSet) {
-    log::info!("Stored pairs {}", pair_created_events.items.len());
+    log::info!("Stored events {}", pair_created_events.items.len());
     for event in pair_created_events.items {
         output.set(
             0,
@@ -63,26 +69,36 @@ fn store_pair_created_event(pair_created_events: uniswap_v2::PairCreatedEvents, 
 }
 
 #[substreams::handlers::map]
-fn map_pair(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<uniswap_v2::Pairs, substreams::errors::Error> {
-    let mut pairs = uniswap_v2::Pairs { items: vec![] };
+fn map_pools(pair_created_events: uniswap_v2::PairCreatedEvents) -> Result<dex_amm::Pools, substreams::errors::Error> {
+    let mut pools = dex_amm::Pools { items: vec![] };
 
     for event in pair_created_events.items {
-        match erc20::get_erc20_token(&event.token0) {
-            None => {
-                continue;
-            }
-            Some(token) => {
-                log::info!("token name: {}", token.name);
-            }
-        }
+        let token0 = erc20::get_erc20_token(&event.token0);
+        let token1 = erc20::get_erc20_token(&event.token1);
 
-        pairs.items.push(uniswap_v2::Pair {
-            name: event.pair.clone(),
-            token0: event.token0,
-            token1: event.token1,
-            address: event.pair.clone(),
-        })
+        if let (Some(token0), Some(token1)) = (token0, token1) {
+            pools.items.push(dex_amm::Pool {
+                name: format!("Uniswap LP: {} / {}", token0.symbol, token1.symbol),
+                address: event.pair,
+                input_tokens: vec![],
+                total_value_locked: "0".to_string(),
+            })
+        } else {
+            log::info!("Failed to fetch token for {} {}", &event.token0, &event.token1);
+        }
     }
 
-    Ok(pairs)
+    Ok(pools)
+}
+
+#[substreams::handlers::store]
+fn store_pools(pools: dex_amm::Pools, output: store::StoreSet) {
+    log::info!("Stored pools {}", pools.items.len());
+    for event in pools.items {
+        output.set(
+            0,
+            &event.address,
+            &proto::encode(&event).unwrap(),
+        );
+    }
 }
