7cce456f96d04ec419ed1f7c6a2c2786a7cce5b6
0xbe1 <101405096+0xbe1@users.noreply.github.com>
2022-09-01 12:49:43 +0800

    fix(substreams-helper,uniswap-v2): build (#27)

--- substreams-helper/src/erc20.rs
+++ substreams-helper/src/erc20.rs
@@ -1,6 +1,7 @@
 use bigdecimal::BigDecimal;
 use num_bigint::BigInt;
 use substreams_ethereum::pb::eth as ethpb;
+use substreams::Hex;
 
 use crate::{abi, math, utils};
 
@@ -11,18 +12,17 @@ pub struct Erc20Token {
     pub decimals: u64,
 }
 
-pub fn get_erc20_token(token_address: &String) -> Option<Erc20Token> {
+pub fn get_erc20_token(token_address: Vec<u8>) -> Option<Erc20Token> {
     use abi::erc20::functions;
 
-    let token_address_vu8 = hex::decode(token_address).unwrap();
-    let name_res = functions::Name {}.call(token_address_vu8.clone());
-    let symbol_res = functions::Symbol {}.call(token_address_vu8.clone());
-    let decimals_res = functions::Decimals {}.call(token_address_vu8.clone());
+    let name_res = functions::Name {}.call(token_address.clone());
+    let symbol_res = functions::Symbol {}.call(token_address.clone());
+    let decimals_res = functions::Decimals {}.call(token_address.clone());
 
     if let (Some(name), Some(symbol), Some(decimals)) = (name_res, symbol_res, decimals_res) {
         Some(
             Erc20Token {
-                address: token_address.to_string(),
+                address: Hex::encode(token_address),
                 name,
                 symbol,
                 decimals: decimals.as_u64(),
