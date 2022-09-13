b73046aa95401d0990f1be52964ff3a90123348b
Ryan Tate <ryan.michael.tate@gmail.com>
2022-09-13 09:21:32 -0700

    substreams-helper: add total_supply to erc20 token
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- substreams-helper/src/erc20.rs
+++ substreams-helper/src/erc20.rs
@@ -1,7 +1,7 @@
 use bigdecimal::BigDecimal;
 use num_bigint::BigInt;
-use substreams_ethereum::pb::eth as ethpb;
 use substreams::Hex;
+use substreams_ethereum::pb::eth as ethpb;
 
 use crate::{abi, math, utils};
 
@@ -10,6 +10,7 @@ pub struct Erc20Token {
     pub name: String,
     pub symbol: String,
     pub decimals: u64,
+    pub total_supply: u64,
 }
 
 pub fn get_erc20_token(token_address: Vec<u8>) -> Option<Erc20Token> {
@@ -18,16 +19,18 @@ pub fn get_erc20_token(token_address: Vec<u8>) -> Option<Erc20Token> {
     let name_res = functions::Name {}.call(token_address.clone());
     let symbol_res = functions::Symbol {}.call(token_address.clone());
     let decimals_res = functions::Decimals {}.call(token_address.clone());
+    let total_supply_res = functions::Decimals {}.call(token_address.clone());
 
-    if let (Some(name), Some(symbol), Some(decimals)) = (name_res, symbol_res, decimals_res) {
-        Some(
-            Erc20Token {
-                address: Hex::encode(token_address),
-                name,
-                symbol,
-                decimals: decimals.as_u64(),
-            }
-        )
+    if let (Some(name), Some(symbol), Some(decimals), Some(total_supply)) =
+        (name_res, symbol_res, decimals_res, total_supply_res)
+    {
+        Some(Erc20Token {
+            address: Hex::encode(token_address),
+            name,
+            symbol,
+            decimals: decimals.as_u64(),
+            total_supply: total_supply.as_u64(),
+        })
     } else {
         None
     }
