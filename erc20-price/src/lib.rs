35cfcdf955fe7fea8fdfb623b4f0fbfeeab8f78f
Ryan Tate <ryan.michael.tate@gmail.com>
2022-09-13 09:16:38 -0700

    feat(erc20-price): add token address to proto definition
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- erc20-price/src/lib.rs
+++ erc20-price/src/lib.rs
@@ -1,25 +1,25 @@
 use std::collections::HashSet;
 
 use hex_literal::hex;
-use substreams::{Hex, log};
-use substreams_ethereum::{Event as EventTrait, pb::eth::v2 as eth};
+use substreams::{log, Hex};
+use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
 
 use pb::erc20_price::{Erc20Price, Erc20Prices};
 use substreams_helper::price;
 use substreams_helper::types::Network;
 
-mod pb;
 mod abi;
+mod pb;
 
 #[substreams::handlers::map]
-fn map_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error> {
+pub fn map_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error> {
     let mut erc20_tokens = HashSet::new();
     for log in block.logs() {
         if let Some(_) = abi::erc20::events::Transfer::match_and_decode(log) {
             let erc20_token = log.log.clone().address;
             erc20_tokens.insert(Hex::encode(erc20_token));
         }
-    };
+    }
     let mut prices = Erc20Prices { items: vec![] };
     for erc20_token in erc20_tokens {
         let erc20_token = Hex::decode(erc20_token).unwrap();
@@ -27,8 +27,9 @@ fn map_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error
         prices.items.push(Erc20Price {
             block_number: block.number,
             price_usd: token_price.to_string(),
+            token_address: erc20_token.clone(),
         });
         log::info!("token {} price {}", Hex(erc20_token), token_price);
-    };
+    }
     Ok(prices)
 }
