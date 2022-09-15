24d652471767b811617448e71edc783d02d85564
Ryan Tate <ryan.michael.tate@gmail.com>
2022-09-15 07:41:16 -0700

    change double to string for floating points, use BigDecimal
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- erc20-market-cap/src/lib.rs
+++ erc20-market-cap/src/lib.rs
@@ -6,13 +6,11 @@ use substreams_ethereum::pb::eth::v2 as eth;
 use pb::erc20_market_cap::{Erc20MarketCap, Erc20MarketCaps};
 use pb::erc20_price::{Erc20Price, Erc20Prices};
 use substreams_helper::erc20::{self, Erc20Token};
-use substreams_helper::types::Network;
+use substreams_helper::{math, types::Network};
 
 #[substreams::handlers::map]
 fn map_market_cap(prices: Erc20Prices) -> Result<Erc20MarketCaps, substreams::errors::Error> {
     let mut items = vec![];
-
-    log::info!("prices: {:?}", prices);
 
     for Erc20Price {
         price_usd,
@@ -20,31 +18,26 @@ fn map_market_cap(prices: Erc20Prices) -> Result<Erc20MarketCaps, substreams::er
         ..
     } in prices.items.iter()
     {
-        let price = price_usd
-            .parse::<f64>()
-            .map_err(|e| substreams::errors::Error::Unexpected(e.to_string()))?;
+        let price = math::decimal_from_str(price_usd);
 
         let Erc20Token { total_supply, .. } = erc20::get_erc20_token(token_address.to_vec())
-            .ok_or(substreams::errors::Error::Unexpected(String::from(
-                "Failed to get token info",
+            .ok_or(substreams::errors::Error::Unexpected(format!(
+                "Failed to get token info for address: 0x{}",
+                Hex::encode(token_address)
             )))?;
 
-        let market_cap = price * (total_supply as f64);
+        let market_cap = price.clone() * math::decimal_from_str(&total_supply.to_string());
 
         let item = Erc20MarketCap {
-            price,
-            total_supply,
-            market_cap,
+            price: format!("{:.7}", price),
+            total_supply: total_supply.to_string(),
+            market_cap: format!("{:.7}", market_cap),
         };
-
-        log::info!(
-            "token {} market cap {}",
-            Hex(token_address.to_vec()),
-            market_cap
-        );
 
         items.push(item);
     }
+
+    log::info!("Market Caps: {:?}", items);
 
     Ok(Erc20MarketCaps { items })
 }
