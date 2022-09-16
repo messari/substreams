0023d4fd27faa56ca76ad069c1b3b219bf353320
Ryan Tate <ryan.michael.tate@gmail.com>
2022-09-16 09:06:00 -0700

    feat(erc20 market cap): add example for erc20-price spkg (#55)
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

---
+++ erc20-market-cap/src/lib.rs
@@ -0,0 +1,45 @@
+mod pb;
+
+use substreams::{log, Hex};
+use substreams_ethereum::pb::eth::v2 as eth;
+
+use pb::erc20_market_cap::{Erc20MarketCap, Erc20MarketCaps};
+use pb::erc20_price::{Erc20Price, Erc20Prices};
+use substreams_helper::erc20::{self, Erc20Token};
+use substreams_helper::{math, types::Network};
+
+#[substreams::handlers::map]
+fn map_market_cap(prices: Erc20Prices) -> Result<Erc20MarketCaps, substreams::errors::Error> {
+    let mut items = vec![];
+
+    for Erc20Price {
+        price_usd,
+        token_address,
+        ..
+    } in prices.items.iter()
+    {
+        let price = math::decimal_from_str(price_usd);
+        let token_address_hex = format!("0x{}", Hex::encode(token_address));
+
+        let Erc20Token { total_supply, .. } = erc20::get_erc20_token(token_address.to_vec())
+            .ok_or(substreams::errors::Error::Unexpected(format!(
+                "Failed to get token info for address: {}",
+                token_address_hex
+            )))?;
+
+        let market_cap = price.clone() * math::decimal_from_str(&total_supply.to_string());
+
+        let item = Erc20MarketCap {
+            price: format!("{:.7}", price),
+            total_supply: total_supply.to_string(),
+            market_cap: format!("{:.7}", market_cap),
+            token_address: token_address_hex,
+        };
+
+        items.push(item);
+    }
+
+    log::info!("Market Caps: {:?}", items);
+
+    Ok(Erc20MarketCaps { items })
+}
--- erc20-price/src/lib.rs
+++ erc20-price/src/lib.rs
@@ -1,15 +1,15 @@
 use std::collections::HashSet;
 
 use hex_literal::hex;
-use substreams::{Hex, log};
-use substreams_ethereum::{Event as EventTrait, pb::eth::v2 as eth};
+use substreams::{log, Hex};
+use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
 
 use pb::erc20_price::{Erc20Price, Erc20Prices};
 use substreams_helper::price;
 use substreams_helper::types::Network;
 
-mod pb;
 mod abi;
+mod pb;
 
 #[substreams::handlers::map]
 fn map_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error> {
@@ -19,16 +19,22 @@ fn map_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error
             let erc20_token = log.log.clone().address;
             erc20_tokens.insert(Hex::encode(erc20_token));
         }
-    };
+    }
     let mut prices = Erc20Prices { items: vec![] };
     for erc20_token in erc20_tokens {
-        let erc20_token = Hex::decode(erc20_token).unwrap();
-        let token_price = price::get_price(Network::Ethereum, erc20_token.clone()).unwrap();
+        let erc20_token = Hex::decode(erc20_token).map_err(|e| {
+            substreams::errors::Error::Unexpected(format!("Failed to decode erc20 token: {}", e))
+        })?;
+        let token_price =
+            price::get_price(Network::Ethereum, erc20_token.clone()).map_err(|e| {
+                substreams::errors::Error::Unexpected(format!("Failed to get price: {}", e))
+            })?;
         prices.items.push(Erc20Price {
             block_number: block.number,
             price_usd: token_price.to_string(),
+            token_address: erc20_token.clone(),
         });
         log::info!("token {} price {}", Hex(erc20_token), token_price);
-    };
+    }
     Ok(prices)
 }
