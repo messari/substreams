ecce35eb51fb37409f7907f1d94f819b1eff899e
Vincent Wen <wensi.isnew@gmail.com>
2022-09-30 15:01:07 -0700

    feature(erc20-holdings): substream for aggregating erc20 token balances (#87)
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>
    Co-authored-by: Ryan Tate <ryan.michael.tate@gmail.com>

---
+++ erc20-holdings/src/lib.rs
@@ -0,0 +1,53 @@
+#[rustfmt::skip]
+pub mod abi;
+#[rustfmt::skip]
+pub mod pb;
+
+use substreams::{hex, log, proto, store, Hex};
+use substreams_ethereum::{pb::eth as pbeth, Event, NULL_ADDRESS};
+
+use substreams_helper::types::Address;
+
+use pb::erc20::v1 as erc20;
+
+/// Extracts transfer events from the contract
+#[substreams::handlers::map]
+fn map_block_to_transfers(
+    block: pbeth::v2::Block,
+) -> Result<erc20::TransferEvents, substreams::errors::Error> {
+    // NOTE: Update TRACKED_CONTRACT to the address of the contract you want to track
+    const TRACKED_CONTRACT: Address = hex!("5a98fcbea516cf06857215779fd812ca3bef1b32"); // LDO
+
+    let mut transfer_events = erc20::TransferEvents { items: vec![] };
+
+    for log in block.logs() {
+        if let Some(event) = abi::erc20::events::Transfer::match_and_decode(log) {
+            if log.address() != TRACKED_CONTRACT {
+                continue;
+            }
+
+            transfer_events.items.push(erc20::TransferEvent {
+                tx_hash: Hex(log.receipt.transaction.clone().hash).to_string(),
+                log_index: log.index(),
+                token_address: Hex(log.address()).to_string(),
+                from: Hex(event.from).to_string(),
+                to: Hex(event.to).to_string(),
+                amount: event.value.to_string(),
+            })
+        }
+    }
+
+    Ok(transfer_events)
+}
+
+#[substreams::handlers::store]
+fn store_transfers(transfers: erc20::TransferEvents, output: store::StoreSet) {
+    log::info!("Stored events {}", transfers.items.len());
+    for event in transfers.items {
+        output.set(
+            0,
+            Hex::encode(&event.token_address),
+            &proto::encode(&event).unwrap(),
+        );
+    }
+}
--- uniswap-v2/src/pb.rs
+++ uniswap-v2/src/pb.rs
@@ -1,3 +1,4 @@
-pub mod uniswap;
 pub mod common;
 pub mod dex_amm;
+pub mod uniswap;
+pub mod erc20;
