44fd42182ebe92b8d31f012b2c27c6625e19a3b3
Vincent Wen <wensi.isnew@gmail.com>
2022-11-01 22:13:35 -0700

    parse and store chainlink aggregator contracts

---
+++ erc20-price/src/keyer.rs
@@ -0,0 +1,3 @@
+pub fn chainlink_aggregator_key(address: &String) -> String {
+    format!("chainlink_aggregator:{}", address)
+}
--- erc20-price/src/lib.rs
+++ erc20-price/src/lib.rs
@@ -5,10 +5,18 @@ pub mod pb;
 
 use hex_literal::hex;
 use pb::erc20_price::v1::{Erc20Price, Erc20Prices};
+use pb::chainlink::v1::Aggregator;
 use substreams::{log, Hex};
-use substreams_ethereum::pb::eth::v2 as eth;
+use substreams::store::StoreNew;
+use substreams::store::StoreSetProto;
+use substreams::store::{StoreAdd, StoreGet, StoreSet};
+use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
 use substreams_helper::price;
 use substreams_helper::types::Network;
+use std::collections::HashSet;
+use crate::abi::access_controlled_aggregator;
+
+mod keyer;
 
 #[substreams::handlers::map]
 fn map_eth_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error> {
@@ -38,4 +46,62 @@ fn map_price_for_tokens(
     }
 
     Ok(prices)
+}
+
+#[substreams::handlers::map]
+fn map_chainlink_eth_price(
+    block: eth::Block,
+) -> Result<Erc20Prices, substreams::errors::Error> {
+    let mut prices = Erc20Prices { items: vec![] };
+
+    for log in block.logs() {
+        if let Some(event) = access_controlled_aggregator::events::AnswerUpdated::match_and_decode(log) {
+            let address = log.address();
+            let current = event.current;
+            log::info!("Address: {}, pair: {}, price: {}", Hex(address).to_string(), 0, current);
+        }
+    }
+
+    Ok(prices)
+}
+
+#[substreams::handlers::store]
+fn store_chainlink_aggregator(block: eth::Block, output: StoreSetProto<Aggregator>) {
+    let mut set = HashSet::new();
+
+    for log in block.logs() {
+        if access_controlled_aggregator::events::OracleAdminUpdated::match_log(&log.log) {
+            let address = Hex(log.address()).to_string();
+            if set.contains(&address) {
+                continue;
+            }
+
+            let decimals = access_controlled_aggregator::functions::Decimals {}
+                .call(log.address().to_vec()).unwrap();
+            let description = access_controlled_aggregator::functions::Description {}
+                .call(log.address().to_vec()).unwrap();
+
+            let trimmed = str::replace(&description, "\"", "");
+            let base_quote: Vec<&str> = trimmed.split(" / ").collect();
+            if base_quote.len() != 2 {
+                log::info!("Unexpected description {}", description);
+                continue;
+            }
+
+            let aggregator = Aggregator {
+                address: address.clone(),
+                description: description.clone(),
+                base: base_quote[0].to_string(),
+                quote: base_quote[1].to_string(),
+                decimals: decimals.to_u64()
+            };
+            output.set(0, keyer::chainlink_aggregator_key(&address), &aggregator);
+            set.insert(address);
+        }
+    }
+}
+
+#[substreams::handlers::store]
+fn store_chainlink_eth_price(block: eth::Block, output: StoreSetProto<Erc20Price>) {
+    // output.set(0, "", );
 }
