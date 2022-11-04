3b9982bf926747974ae9a3b466c321ef9c6a4c32
Vincent Wen <wensi.isnew@gmail.com>
2022-11-03 17:44:35 -0700

    parse and store chainlink price

--- erc20-price/src/keyer.rs
+++ erc20-price/src/keyer.rs
@@ -1,3 +1,7 @@
 pub fn chainlink_aggregator_key(address: &String) -> String {
     format!("chainlink_aggregator:{}", address)
 }
+
+pub fn chainlink_asset_key(asset_address: &String) -> String {
+    format!("chainlink_price:{}", asset_address)
+}
--- erc20-price/src/lib.rs
+++ erc20-price/src/lib.rs
@@ -7,16 +7,23 @@ use hex_literal::hex;
 use pb::erc20_price::v1::{Erc20Price, Erc20Prices};
 use pb::chainlink::v1::Aggregator;
 use substreams::{log, Hex};
+use substreams::scalar::BigInt;
 use substreams::store::StoreNew;
-use substreams::store::StoreSetProto;
+use substreams::store::{StoreSetProto, StoreGetProto};
 use substreams::store::{StoreAdd, StoreGet, StoreSet};
 use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
 use substreams_helper::price;
 use substreams_helper::types::Network;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use crate::abi::access_controlled_aggregator;
 
 mod keyer;
+
+lazy_static::lazy_static!{
+    static ref TOKEN_ADDRESS: HashMap<&'static str, &'static str> = vec![
+        ("ETH", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
+    ].into_iter().collect();
+}
 
 #[substreams::handlers::map]
 fn map_eth_price(block: eth::Block) -> Result<Erc20Prices, substreams::errors::Error> {
@@ -54,24 +61,23 @@ fn map_chainlink_eth_price(
 ) -> Result<Erc20Prices, substreams::errors::Error> {
     let mut prices = Erc20Prices { items: vec![] };
 
-    for log in block.logs() {
-        if let Some(event) = access_controlled_aggregator::events::AnswerUpdated::match_and_decode(log) {
-            let address = log.address();
-            let current = event.current;
-            log::info!("Address: {}, pair: {}, price: {}", Hex(address).to_string(), 0, current);
-        }
-    }
-
     Ok(prices)
 }
 
 #[substreams::handlers::store]
-fn store_chainlink_aggregator(block: eth::Block, output: StoreSetProto<Aggregator>) {
+fn store_chainlink_aggregator(
+    block: eth::Block,
+    output: StoreSetProto<Aggregator>
+) {
     let mut set = HashSet::new();
 
     for log in block.logs() {
+        // Use `OracleAdminUpdated` as a proxy to contract deployment as it's
+        // one of the first events emitted by the Aggregator contract
         if access_controlled_aggregator::events::OracleAdminUpdated::match_log(&log.log) {
             let address = Hex(log.address()).to_string();
+
+            // We only need to update the store once as the fields are immutable
             if set.contains(&address) {
                 continue;
             }
@@ -84,7 +90,7 @@ fn store_chainlink_aggregator(block: eth::Block, output: StoreSetProto<Aggregato
             let trimmed = str::replace(&description, "\"", "");
             let base_quote: Vec<&str> = trimmed.split(" / ").collect();
             if base_quote.len() != 2 {
-                log::info!("Unexpected description {}", description);
+                log::info!("Unexpected description: {}", description);
                 continue;
             }
 
@@ -102,6 +108,37 @@ fn store_chainlink_aggregator(block: eth::Block, output: StoreSetProto<Aggregato
 }
 
 #[substreams::handlers::store]
-fn store_chainlink_eth_price(block: eth::Block, output: StoreSetProto<Erc20Price>) {
-    // output.set(0, "", );
+fn store_chainlink_price(
+    block: eth::Block,
+    store: StoreGetProto<Aggregator>,
+    output: StoreSetProto<Erc20Price>
+) {
+    for log in block.logs() {
+        if let Some(event) = access_controlled_aggregator::events::AnswerUpdated::match_and_decode(log) {
+            let aggregator_address = Hex(log.address()).to_string();
+            let current: BigInt = event.current;
+            match store.get_last(keyer::chainlink_aggregator_key(&aggregator_address)) {
+                None => continue,
+                Some(aggregator) => {
+                    if aggregator.quote == "USD" || aggregator.quote == "DAI" || aggregator.quote == "USDC" || aggregator.quote == "USDT" {
+                        let price = current.to_decimal(aggregator.decimals);
+                        log::info!("Address: {}, desc: {}, price: {}", aggregator_address, aggregator.description, price);
+
+                        let token_address = TOKEN_ADDRESS.get("ETH").unwrap().to_string();
+
+                        let erc20price = Erc20Price {
+                            block_number: block.number,
+                            price_usd: price.to_string(),
+                            token_address: aggregator.description.clone()
+                        };
+                        output.set(
+                            0,
+                            keyer::chainlink_asset_key(&aggregator.description.clone()),
+                            &erc20price
+                        );
+                    }
+                }
+            }
+        }
+    }
 }
