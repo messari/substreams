6a0ec2c202b3cce4b90acd488d223ead7feaa8bd
dmelotik <dylanmelotik@gmail.com>
2022-12-14 18:33:39 -0700

    set output

--- sol-balance/src/lib.rs
+++ sol-balance/src/lib.rs
@@ -1,45 +1,24 @@
 use substreams::log;
 use substreams::store::StoreSet;
 use substreams_solana::pb::sol as solana;
-// use bs58;
+use bs58;
 
 #[substreams::handlers::store]
 fn store_balance(block: solana::v1::Block, output: StoreSet) {
-    log::info!("block hash: {}", block.previous_blockhash);
-    log::info!("block hash: {}", block.parent_slot);
-
-    // TODO: try to get balances in this for loop
-
     for tx in block.transactions {
         if let Some(meta) = tx.meta {
             if let Some(_) = meta.err {
                 continue;
             }
-            log::info!("bal len: {}", meta.post_balances.len());
-
-            for bal in meta.post_balances {
+            log::info!("bal len: {}", &meta.post_balances.len()); // TODO: remove
+            for i in 0..meta.post_balances.len() {
                 // TODO: I don't think this is the right address, but I am struggling to figure out another way
-
-                // let change_address =
-                //
-                // output.set(
-                //     0,
-                //
-                // )
-            }
-            log::info!("{}", meta.loaded_writable_addresses.len());
-            log::info!("{}", meta.post_balances.len());
-            // for bal in meta.post_balances {
-            //     log::info!("post bal: {}", bal);
-            // }
-            if let Some(transaction) = tx.transaction {
-                if let Some(msg) = transaction.message {
-                    log::info!("acct len: {}", msg.account_keys.len());
-                    for accts in msg.account_keys {
-                        // let program_id = &msg.account_keys[inst.program_id_index as usize];
-                        // log::info!("acc: {}", bs58::encode(program_id).into_string());
-                    }
-                }
+                let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
+                output.set(
+                    i as u64,
+                    account_id,
+                    &meta.post_balances[i] // TODO: not sure why this wants a &Vec<u8>
+                );
             }
         }
     }
