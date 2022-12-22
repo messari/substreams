def13f0aadb6fc30b46b3038d3cb03e56cf07966
dmelotik <dylanmelotik@gmail.com>
2022-12-22 11:05:05 -0700

    encode to vec and update readmes

--- sol-balance/README.md
+++ sol-balance/README.md
@@ -8,3 +8,8 @@ This substream is designed to store the SOL balance of every account.
 make codegen
 make run
 ```
+
+### Notes
+
+- Currently [`transaction.loaded_writable_addresses`](https://github.com/streamingfast/firehose-solana/blob/develop/proto/sf/solana/type/v1/type.proto#L65) is an unknown field on firesol blocks.
+- 
--- sol-balance/src/lib.rs
+++ sol-balance/src/lib.rs
@@ -1,7 +1,7 @@
-use substreams::log;
 use substreams::store::StoreSet;
 use substreams_solana::pb::sol as solana;
 use bs58;
+use prost::Message;
 
 #[substreams::handlers::store]
 fn store_balance(block: solana::v1::Block, output: StoreSet) {
@@ -10,14 +10,13 @@ fn store_balance(block: solana::v1::Block, output: StoreSet) {
             if let Some(_) = meta.err {
                 continue;
             }
-            log::info!("bal len: {}", &meta.post_balances.len()); // TODO: remove
             for i in 0..meta.post_balances.len() {
                 // TODO: I don't think this is the right address, but I am struggling to figure out another way
                 let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
                 output.set(
                     i as u64,
                     account_id,
-                    &meta.post_balances[i] // TODO: not sure why this wants a &Vec<u8>
+                    &meta.post_balances[i].encode_to_vec()
                 );
             }
         }
