3a4cbbfc7bc7f44e277876bb3b2b6b68f9075728
dmelotik <dylanmelotik@gmail.com>
2023-01-12 12:38:05 -0700

    fix sol balance build setup

--- sol-balance/src/lib.rs
+++ sol-balance/src/lib.rs
@@ -1,27 +1,36 @@
-use substreams::store::StoreSet;
-use crate::pb::token::v1::Token;
-use pb::token::v1 as proto;
-use substreams_solana::pb::sol as solana;
+use crate::pb::sol as token;
 use bs58;
+// use pb::sol::Account;
 use prost::Message;
+use substreams::store::StoreSet;
 use substreams_solana::pb;
+use substreams_solana::pb::sol as solana;
 
-#[substreams::handlers::map]
-fn map(block: solana::v1::Block) -> Result<proto:> {
-    for tx in block.transactions {
-        if let Some(meta) = tx.meta {
-            if let Some(_) = meta.err {
-                continue;
-            }
-            for i in 0..meta.post_balances.len() {
-                // TODO: I don't think this is the right address, but I am struggling to figure out another way
-                let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
-                output.set(
-                    i as u64,
-                    account_id,
-                    &meta.post_balances[i].encode_to_vec()
-                );
-            }
-        }
-    }
+#[substreams::handlers::store]
+fn store_balance(block: solana::v1::Block, output: StoreSet) {
+    output.set(
+        0,
+        "block".to_string(),
+        &format!("{:?}", block).as_bytes().to_vec(),
+    );
 }
+
+// #[substreams::handlers::map]
+// fn map(block: solana::v1::Block) -> Result<Account:> {
+//     for tx in block.transactions {
+//         if let Some(meta) = tx.meta {
+//             if let Some(_) = meta.err {
+//                 continue;
+//             }
+//             for i in 0..meta.post_balances.len() {
+//                 // TODO: I don't think this is the right address, but I am struggling to figure out another way
+//                 let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
+//                 output.set(
+//                     i as u64,
+//                     account_id,
+//                     &meta.post_balances[i].encode_to_vec()
+//                 );
+//             }
+//         }
+//     }
+// }
