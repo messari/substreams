61d4b083888abb553dca82a67889d4045217385d
dmelotik <dylanmelotik@gmail.com>
2023-01-12 18:39:25 -0700

    change to map module

--- sol-balance/src/lib.rs
+++ sol-balance/src/lib.rs
@@ -1,36 +1,37 @@
-use crate::pb::sol as token;
+use crate::pb::sol::v1 as token; // TODO: I can't get this import to work :(
 use bs58;
-// use pb::sol::Account;
 use prost::Message;
 use substreams::store::StoreSet;
 use substreams_solana::pb;
 use substreams_solana::pb::sol as solana;
 
-#[substreams::handlers::store]
-fn store_balance(block: solana::v1::Block, output: StoreSet) {
-    output.set(
-        0,
-        "block".to_string(),
-        &format!("{:?}", block).as_bytes().to_vec(),
-    );
-}
+#[substreams::handlers::map]
+fn map_balances(block: solana::v1::Block) -> Result<token::Accounts, substreams::errors::Error> {
+    let accounts = vec![];
+    for tx in block.transactions {
+        if let Some(meta) = tx.meta {
+            if let Some(_) = meta.err {
+                continue;
+            }
+            for i in 0..meta.post_balances.len() {
+                let new_balance = &meta.post_balances[i].encode_to_vec();
+                let new_token_balance = vec![token::Balance {
+                    token: get_sol_token(),
+                    balance: new_balance,
+                    block_number: block.block_height.block_height,
+                    timestamp: block.block_time.timestamp,
+                }];
+                // TODO: Not sure if this is the right place to get the address
+                // AND loaded_writeable_addresses is not being built in the proto stucts
+                // let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
+                let account = token::Account {
+                    account_id: "0x0",
+                    balances: new_token_balance,
+                };
+                accounts.push(account);
+            }
+        }
+    }
 
-// #[substreams::handlers::map]
-// fn map(block: solana::v1::Block) -> Result<Account:> {
-//     for tx in block.transactions {
-//         if let Some(meta) = tx.meta {
-//             if let Some(_) = meta.err {
-//                 continue;
-//             }
-//             for i in 0..meta.post_balances.len() {
-//                 // TODO: I don't think this is the right address, but I am struggling to figure out another way
-//                 let account_id = bs58::encode(&meta.loaded_writable_addresses[i]).into_string();
-//                 output.set(
-//                     i as u64,
-//                     account_id,
-//                     &meta.post_balances[i].encode_to_vec()
-//                 );
-//             }
-//         }
-//     }
-// }
+    Ok(token::Accounts { items: accounts })
+}
