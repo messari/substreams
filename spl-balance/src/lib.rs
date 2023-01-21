eadcc04b000411dbeffd13065faa903e8105877d
dmelotik <dylanmelotik@gmail.com>
2023-01-20 22:38:39 -0700

    do map_balances

--- spl-balance/src/lib.rs
+++ spl-balance/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod pb;
 
+use bs58;
 use num_bigint;
 use pb::sol_token::v1 as proto;
 use substreams::scalar::BigInt;
 use substreams::store::{StoreNew, StoreSet, StoreSetRaw};
-use substreams::{store, Hex, log};
+use substreams::{log, store, Hex};
 use substreams_solana::pb::sol as solana;
-use bs58;
 
 // Map SPL token balance changes
 #[substreams::handlers::map]
@@ -15,7 +15,6 @@ fn map_balances(
 ) -> Result<proto::BalanceChanges, substreams::errors::Error> {
     log::info!("extracting SPL balance changes");
     let mut balance_changes = proto::BalanceChanges { items: vec![] };
-    let mut index = 0;
 
     for trx in block.transactions {
         if let Some(meta) = trx.meta {
@@ -26,11 +25,36 @@ fn map_balances(
                 log::info!("{}", bs58::encode(&transaction.signatures[0]).into_string())
             }
 
-            // let vec_string: String = meta.pre_balances.iter().map(|x| x.to_string()).collect::<Vec<String>>().join("");
-            log::info!("pre_balances: {} {} {} {}", meta.pre_balances.len(), meta.post_balances.len(), meta.pre_token_balances.len(), meta.post_token_balances.len());
+            for i in 0..meta.pre_token_balances.len() {
+                let pre_balance = &meta.pre_token_balances[i];
+                let post_balance = &meta.post_token_balances[i];
+                let mut pre_balance_amount = "";
+                let mut post_balance_amount = "";
+                // pre.owner = user address
+                // pre.mint = token address
+                if let Some(pre_token_amount) = &pre_balance.ui_token_amount {
+                    pre_balance_amount = pre_token_amount.amount;
+                }
+                if let Some(post_token_amount) = &post_balance.ui_token_amount {
+                    post_balance_amount = post_token_amount.amount;
+                }
+
+                balance_changes.push(proto::TokenBalance {
+                    token: proto::Token {}, // TODO: this should be fed from store_tokens using pre.mint
+                    transaction_id: bs58::encode(&transaction.signatures[0]).into_string(),
+                    block_height: block.block_height.block_height,
+                    account: pre_balance.owner,
+                    pre_balance: pre_balance_amount,
+                    post_balance: post_balance_amount,
+                });
+            }
         }
     }
 
-
     Ok(balance_changes)
+}
+
+#[substreams::handlers::store]
+fn store_tokens(block: solana::v1::Block, output: store::StoreSet) {
+
 }
