340028b71c2de7ca0c57367c4bdbaf5521e51b14
dmelotik <dylanmelotik@gmail.com>
2023-01-24 21:54:30 -0700

    update versions, attempt to get solana-sample to work

--- solana-sample/src/lib.rs
+++ solana-sample/src/lib.rs
@@ -1,7 +1,8 @@
-use substreams::{log, store};
+use substreams::store;
+use substreams::store::StoreSet;
 use substreams_solana::pb::sol as solana;
 
 #[substreams::handlers::store]
-fn store_test(block: solana::v1::Block, _output: store::StoreSet) {
+fn store_test(block: solana::v1::Block, _output: StoreSet) {
     _output.set(0, "block".to_string(), &format!("{:?}", block).as_bytes().to_vec());
 }
