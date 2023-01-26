f730c0f9e16bf52983a431925994e4a7d15d02b4
dmelotik <dylanmelotik@gmail.com>
2023-01-25 20:53:06 -0700

    find accounts in sol balance

--- sol-balance/src/lib.rs
+++ sol-balance/src/lib.rs
@@ -1,26 +1,40 @@
-use substreams_solana::pb::sol as solana;
-use substreams_helper::token::get_sol_token;
+use bs58;
 use substreams_helper::pb::sol_token::v1 as proto;
+use substreams_helper::token::get_sol_token;
+use substreams_solana::pb::sol as solana;
 
 #[substreams::handlers::map]
-fn map_balances(block: solana::v1::Block) -> Result<proto::BalanceChanges, substreams::errors::Error> {
+fn map_balances(
+    block: solana::v1::Block,
+) -> Result<proto::BalanceChanges, substreams::errors::Error> {
     let mut balances = vec![];
     for tx in block.transactions {
         if let Some(meta) = tx.meta {
             if let Some(_) = meta.err {
                 continue;
             }
-            for i in 0..meta.post_balances.len() {
-                balances.push(proto::TokenBalance {
-                    token: get_sol_token(),
-                    block_height: block.block_height.as_ref().unwrap().block_height,
-                    address: "TODO".to_string(),
-                    pre_balance: meta.pre_balances[i].to_string(),
-                    post_balance: meta.post_balances[i].to_string()
-                });
+            if let Some(transaction) = tx.transaction {
+                if let Some(msg) = transaction.message {
+                    for i in 0..meta.post_balances.len() {
+                        let account_id: String;
+                        if i < msg.account_keys.len() {
+                            account_id = bs58::encode(&msg.account_keys[i]).into_string();
+                        } else {
+                            account_id = "TODO".to_string();
+                            // TODO: use msg.address_table_lookup table, but it is not avail
+                        }
+                        balances.push(proto::TokenBalance {
+                            token: get_sol_token(),
+                            block_height: block.block_height.as_ref().unwrap().block_height,
+                            address: account_id,
+                            pre_balance: meta.pre_balances[i].to_string(),
+                            post_balance: meta.post_balances[i].to_string(),
+                        });
+                    }
+                }
             }
         }
     }
 
-    Ok(proto::BalanceChanges { items: balances})
+    Ok(proto::BalanceChanges { items: balances })
 }
--- substreams-helper/src/token.rs
+++ substreams-helper/src/token.rs
@@ -2,12 +2,12 @@ use crate::pb::sol_token::v1::TokenAccount;
 
 pub fn get_sol_token() -> Option<TokenAccount> {
     let sol_token = TokenAccount {
-        address: "TODO".to_string(),
+        address: "So11111111111111111111111111111111111111111".to_string(),
         name: "Solana".to_string(),
         symbol: "SOL".to_string(),
-        decimals: 18_u64,
-        owner: "TODO".to_string(),
-        mint: "TODO".to_string()
+        decimals: 9_u64,
+        freeze_authority: "NA".to_string(),
+        mint_authority: "NA".to_string(),
     };
 
     Some(sol_token)
