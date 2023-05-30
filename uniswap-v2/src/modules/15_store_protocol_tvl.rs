39a889a296d35d2e0347446045778fe1e5e52631
Harsh <hagarwal9200@gmail.com>
2023-05-30 18:15:39 +0530

    Fix formatting

--- uniswap-v2/src/modules/15_store_protocol_tvl.rs
+++ uniswap-v2/src/modules/15_store_protocol_tvl.rs
@@ -5,7 +5,10 @@ use crate::store_key::StoreKey;
 use crate::utils::delta_value;
 
 #[substreams::handlers::store]
-pub fn store_protocol_tvl(pool_tvl_deltas: Deltas<DeltaBigDecimal>, output_store: StoreAddBigDecimal) {
+pub fn store_protocol_tvl(
+    pool_tvl_deltas: Deltas<DeltaBigDecimal>,
+    output_store: StoreAddBigDecimal,
+) {
     for delta in pool_tvl_deltas.deltas {
         output_store.add(
             delta.ordinal,
